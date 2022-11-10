1c69a1fb88996c47f840418e695e395ea0de0f16
Cameron Yee <yeec327@gmail.com>
2022-11-10 01:24:52 -0800

    FIxed bug where .stop() would run when nothing was playing, causing it to crash since there is nothing to stop

--- morse-code/src/App.js
+++ morse-code/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SoundButton from "./components/SoundButton.js";
 
 // Node module function that generates a random word.
-const randomWords = require('random-words');
+const randomWords = require("random-words");
 
 // Have to have this globally so that every function can access it and that it is only created once.
 // Every time after, we just want to edit the properties.
@@ -15,18 +15,26 @@ function App() {
   const [morseText, setMorseText] = useState("Hello There");
 
   let createNewSoundInstance = () => {
-    // Stop the previous iteration.
-    morseCodeObj.stop();
+    // Stop the previous iteration only if in the middle of it. Otherwise, can just create a new player without stopping the previous one.
+    // Basically, only stop if something is playing.
+    if (morseCodeObj.getLength() !== 0) {
+      morseCodeObj.stop();
+    }
 
     // To access code from an external js script tag, access it from the window.
     morseCodeObj = new window.jscw({ wpm: wpm, text: morseText });
     morseCodeObj.renderPlayer("player", morseCodeObj);
   };
 
+  let changeWpm = () => {
+    setWpm(25);
+  };
+
+  // Next, make another(?) function (or put it in the same function with a useState bool that determines this) or generate single letters in this function.
   const generateRandomWord = () => {
     let ranWord = randomWords();
     setMorseText(ranWord);
-  }
+  };
 
   // Load in the morse code library that plays the sound.
   useEffect(() => {
@@ -44,17 +52,15 @@ function App() {
     } else {
       createNewSoundInstance();
     }
-    
+
     document.body.appendChild(script);
   }, [wpm, morseText]);
 
-  let changeWpm = () => {
-    setWpm(25);
-  };
-
   return (
     <div className="App">
-      <div onClick={() => generateRandomWord()}><SoundButton /></div>
+      <div onClick={() => generateRandomWord()}>
+        <SoundButton />
+      </div>
       <div id="player"></div>
       <div onClick={() => changeWpm()}>Change to 25 WPM</div>
     </div>
