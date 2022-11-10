1c5b0cfc3ab30f53d8aa140d61e961bf9d120f62
Cameron Yee <yeec327@gmail.com>
2022-11-10 14:42:04 -0800

    Modularized, displayed morse code/text, created random letter button

--- morse-code/src/App.js
+++ morse-code/src/App.js
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Button from "./components/Button.js";
-import styles from './styles/App.module.scss';
+import styles from "./styles/App.module.scss";
+const morseCodeConverter = require("morse-code-converter");
 
 // Node module function that generates a random word.
 const randomWords = require("random-words");
+const commonWpm = [5, 10, 15, 20, 25];
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 // Have to have this globally so that every function can access it and that it is only created once.
 // Every time after, we just want to edit the properties.
@@ -14,6 +44,11 @@ function App() {
   const [isFirstSound, setIsFirstSound] = useState(true);
   const [wpm, setWpm] = useState(5);
   const [morseText, setMorseText] = useState("Cameron Yee");
+  const [morseCode, setMorseCode] = useState(
+    "-.-. .- -- . .-. --- -.   -.-- . ."
+  );
+  const [showMorseCode, setShowMorseCode] = useState(false);
+  const [showMorseText, setShowMorseText] = useState(false);
 
   let createNewSoundInstance = () => {
     // Stop the previous iteration only if in the middle of it. Otherwise, can just create a new player without stopping the previous one.
@@ -27,18 +62,29 @@ function App() {
     morseCodeObj.renderPlayer("player", morseCodeObj);
   };
 
-  let change5Wpm = () => {
-    setWpm(5);
+  let changeWpm = (speed) => {
+    setWpm(speed);
   };
 
-  let change25Wpm = () => {
-    setWpm(25);
-  };
-
-  // Next, make another(?) function (or put it in the same function with a useState bool that determines this) or generate single letters in this function.
   const generateRandomWord = () => {
     let ranWord = randomWords();
     setMorseText(ranWord);
+    let morseCode = morseCodeConverter.textToMorse(ranWord);
+    setMorseCode(morseCode);
+
+    setShowMorseCode(false);
+    setShowMorseText(false);
+  };
+
+  const generateRandomLetter = () => {
+    let ranNumIndex = Math.floor(Math.random() * 26);
+    let letter = alphabet[ranNumIndex]
+    setMorseText(letter);
+    let morseCode = morseCodeConverter.textToMorse(letter);
+    setMorseCode(morseCode);
+
+    setShowMorseCode(false);
+    setShowMorseText(false);
   };
 
   // Load in the morse code library that plays the sound.
@@ -64,21 +110,43 @@ function App() {
   return (
     <div className="App">
       <div className={styles.settings}>
-        <div onClick={() => generateRandomWord()}>
-        <Button text={"Generate Random Word"} />
-      </div>
-      <div onClick={() => change5Wpm()}>
-        <Button text={"5 WPM"} />
-      </div>
-      <div onClick={() => change25Wpm()}>
-        <Button text={"25 WPM"} />
-      </div>
-
+        <div className={styles.clickable} onClick={() => generateRandomWord()}>
+          <Button text={"Random Word"} />
+        </div>
+        <div className={styles.clickable} onClick={() => generateRandomLetter()}>
+          <Button text={"Random Letter"} />
+        </div>
+        {commonWpm.map((speed, key) => {
+          return (
+            <div key={key} onClick={() => changeWpm(speed)} className={styles.clickable}>
+              <Button key={key} text={`${speed} WPM`} />
+            </div>
+          );
+        })}
       </div>
       <div className={styles.player}>
         <div id="player"></div>
       </div>
-      
+
+      {showMorseCode ? (
+        <div onClick={() => setShowMorseCode(false)}>
+          <p className={styles.text}>{morseCode}</p>
+        </div>
+      ) : (
+        <div onClick={() => setShowMorseCode(true)}>
+          <p className={styles.text}>Click to show Morse Code</p>
+        </div>
+      )}
+
+      {showMorseText ? (
+        <div onClick={() => setShowMorseText(false)}>
+          <p className={styles.text}>{morseText}</p>
+        </div>
+      ) : (
+        <div onClick={() => setShowMorseText(true)}>
+          <p className={styles.text}>Click to show Morse Text</p>
+        </div>
+      )}
     </div>
   );
 }
